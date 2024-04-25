512c463bb76212cfc1aa6477b38ec0955bd52451
idilwor <115816447+idilwor@users.noreply.github.com>
2024-04-25 18:49:01 -0400

    hw9 database

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
 FROM eclipse-temurin:17
 WORKDIR /home
-COPY ./target/coffee-order-0.0.1.jar coffee-order.jar
+COPY ./target/lab10-0.0.1-SNAPSHOT.jar coffee-order.jar
 ENTRYPOINT ["java", "-jar", "coffee-order.jar"]
---
+++ src/test/java/edu/iu/habahram/coffeeorder/CoffeeOrderApplicationTests.java
@@ -0,0 +1,18 @@
+package edu.iu.habahram.coffeeorder;
+
+import edu.iu.habahram.coffeeorder.model.*;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class CoffeeOrderApplicationTests {
+
+    // test that easily passes
+    @Test
+    void test1() {
+        assertEquals(1, 1);
+    }
+
+}
